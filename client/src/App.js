dcb8c8bf98279d085d6856fe8deda2f36d6952ff
brightuautumnsky.21 <brightautumnsky.21@gmail.com>
2022-04-14 15:41:41 +0900

    get pins

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,35 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
-import ReactMapGL from "react-map-gl";
+import ReactMapGL, { Marker } from "react-map-gl";
+import axios from "axios";
+import { FaMapPin } from "react-icons/fa";
 import Header from "./components/Header";
+import StyledPopup from "./components/StyledPopup";
 
 function App() {
+  const [pins, setPins] = useState([]);
   const [viewport, setViewport] = useState({
     latitude: 37,
     longitude: 127,
     width: "100vw",
     height: "100vh",
-    zoom: 3,
+    zoom: 4,
   });
   const tk = process.env.REACT_APP_MAPBOX;
 
-  console.log(tk);
+  useEffect(() => {
+    const getPins = async () => {
+      try {
+        const allPins = await axios.get("/pins");
+        setPins(allPins.data);
+        console.log(allPins.data);
+      } catch (e) {
+        console.log(e);
+      }
+    };
+    getPins();
+  }, []);
+
   return (
     <div className="App">
       <Header title="Let's Memo Our Travle" />
@@ -22,7 +38,30 @@ function App() {
           {...viewport}
           mapboxApiAccessToken={tk}
           onViewportChange={(nextViewport) => setViewport(nextViewport)}
-        ></ReactMapGL>
+          mapStyle="mapbox://styles/brightautmnsky21/cl1yk3jux001g14o5fy213k62"
+        >
+          {pins.map((pin, index) => (
+            <>
+              <Marker
+                key={index}
+                latitude={pin.lat}
+                longitude={pin.long}
+                offsetLeft={-3.5 * viewport.zoom}
+                offsetTop={-7 * viewport.zoom}
+              >
+                {
+                  <FaMapPin
+                    style={{
+                      fontSize: viewport.zoom * 10,
+                      color: "seagreen",
+                    }}
+                  />
+                }
+              </Marker>
+              <StyledPopup pins={pins} />
+            </>
+          ))}
+        </ReactMapGL>
       </div>
     </div>
   );
---
+++ client/src/components/StyledPopup.js
@@ -0,0 +1,84 @@
+import React from "react";
+import styled from "styled-components";
+import { Popup } from "react-map-gl";
+import { FaStar } from "react-icons/fa";
+
+const PopupWrapper = styled.div`
+  .info-box {
+    display: flex;
+    flex-direction: column;
+    width: 270px;
+    height: 270px;
+    padding: 12px;
+    justify-content: space-around;
+    h4,
+    p,
+    .stars,
+    .review,
+    .info,
+    .date {
+      margin: 5px 0;
+    }
+    .stars {
+      font-size: 21px;
+      color: gold;
+    }
+    .info {
+      display: flex;
+      flex-direction: column;
+    }
+    .username {
+      font-size: 14px;
+    }
+    .date {
+      font-size: 11px;
+    }
+  }
+
+  label {
+    width: max-content;
+    color: seagreen;
+    font-size: 15px;
+    border-bottom: 1px solid seagreen;
+  }
+`;
+
+const StyledPopup = ({ pins }) => {
+  return (
+    <PopupWrapper>
+      {pins &&
+        pins.map((pin, index) => (
+          <Popup
+            key={index}
+            latitude={pin.lat}
+            longitude={pin.long}
+            closeButton={true}
+            closeOnClick={false}
+            anchor="left"
+          >
+            <div className="info-box">
+              <label>장소</label>
+              <h4 className="place">{pin.title}</h4>
+              <label>후기</label>
+              <div className="review">
+                <p>{pin.desc}</p>
+              </div>
+              <label>별점</label>
+              <div className="stars">
+                <FaStar />
+                <FaStar />
+                <FaStar />
+              </div>
+              <label>정보</label>
+              <div className="info">
+                <span className="username">{pin.username}</span>
+                <span className="date">2022.02.02</span>
+              </div>
+            </div>
+          </Popup>
+        ))}
+    </PopupWrapper>
+  );
+};
+
+export default StyledPopup;
